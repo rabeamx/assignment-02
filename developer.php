b06c5c622bd789070114ee2d9dac8efc5c826bfa
nazmunx <rabeakter02@gmail.com>
2022-04-02 16:44:17 +0600

    upload error fix

--- developer.php
+++ developer.php
@@ -8,49 +8,49 @@ $devs = [
         'disc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit soluta, iusto neque molestias ab sint error maiores sed sunt animi.',
         'photo' => 'https://cdn.powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png'
     ],
-    $devs = [
+    [
         'name' => 'James Arck',
         'age' => 19,
         'skill' => 'Laravel Developer',
         'disc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit soluta, iusto neque molestias ab sint error maiores sed sunt animi.',
         'photo' => 'https://trustjuris.com/wp-content/uploads/2019/05/Warren-Edit-e1499274450548-500x500.jpg'
     ],
-    $devs = [
+    [
         'name' => 'John Doe',
         'age' => 13,
         'skill' => 'MernStack Dev',
         'disc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit soluta, iusto neque molestias ab sint error maiores sed sunt animi.',
         'photo' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5ebI6Jem9IowK2OwLmMqOyw8a3YTDc3wRH9DavprdLsMCMXu8X9LaRJzzkfEPFoSmHUw&usqp=CAU'
     ],
-    $devs = [
+    [
         'name' => 'Nick Jones',
         'age' => 21,
         'skill' => 'Phython Dev',
         'disc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit soluta, iusto neque molestias ab sint error maiores sed sunt animi.',
         'photo' => 'http://prokritipolli.com/wp-content/uploads/2017/04/team1.jpg'
     ],
-    $devs = [
+    [
         'name' => 'Jack Hove',
         'age' => 17,
         'skill' => 'Chainblock Dev',
         'disc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit soluta, iusto neque molestias ab sint error maiores sed sunt animi.',
         'photo' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4P9NJmsYN97EQBE9wFv2a3CfzCuuQD72CT0z0J392Qfyl6Aiq66JKjTsj6a69J40_IUw&usqp=CAU'
     ],
-    $devs = [
+    [
         'name' => 'Richar Hunter',
         'age' => 30,
         'skill' => 'Java Developer',
         'disc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit soluta, iusto neque molestias ab sint error maiores sed sunt animi.',
         'photo' => 'https://energipersona.com/wp-content/uploads/2016/08/team-23.jpg'
     ],
-    $devs = [
+    [
         'name' => 'Paul Harley',
         'age' => 13,
         'skill' => 'PHP Developer',
         'disc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit soluta, iusto neque molestias ab sint error maiores sed sunt animi.',
         'photo' => 'https://psdconverthtml.com/live/convert/upload/big_team_05.jpg'
     ],
-    $devs = [
+    [
         'name' => 'Vicky Hunter',
         'age' => 20,
         'skill' => 'Jquery Developer',
